f101399fceb32d398075b214f117d9c3815ce558
CodeFuGrandmaster <36977159+CodeFuGrandmaster@users.noreply.github.com>
2018-08-30 21:53:46 -0500

    [v0.02] Basic Functionality/Structure

--- carousel.js
+++ carousel.js
@@ -1,3 +1,6 @@
+alert("This is a sample template to show basic functionality. Current colors and borders are for my benefit (the developer) to distinguish what blocks take up what space. Also note that the layouts are not set in stone.");
+
+
 var slideIndex = 1;
 
 function moveSlide(n) {
@@ -16,10 +19,13 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1;}
   if(n < 1) {slideIndex=slides.length;}
   
-  for(i = 0; i < slides.length; i++)
-  {
+  for(i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  
+  // for(i = 0; i < dots.legth; i++) {
+    
+  // }
   
   slides[slideIndex-1].style.display = "block";
 }
